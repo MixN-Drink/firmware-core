2c69d80dc2b54b2483540774fe80dda166da281a
thomasverita <thomaseyen11@gmail.com>
2022-09-29 01:30:44 -0300

    cambio de motor pap a motor de dc

--- lib/Motor/Motor.h
+++ lib/Motor/Motor.h
@@ -1,14 +1,8 @@
 #include <stdint.h>
 #include <Arduino.h>
 
-#define DELAY_ENTRE_PASOS 2
-#define MOTOR_LOOP_TIME 100
-
-#define MOTOR_0_PIN_STEP 54
-#define MOTOR_0_PIN_DIR 55
-#define MOTOR_0_PIN_ENABLE 38
+#define MOTOR_0_PIN 4
 
 void motoresInit(void);
-void prepararMotor(uint8_t numeroDeMotor, uint32_t pasos);
+void prepararMotor(uint8_t numeroDeMotor, uint32_t tiempo);
 void procesarMotores(void);
-void rotarMotor();
--- lib/Surtidor/Surtidor.h
+++ lib/Surtidor/Surtidor.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include "Motor.h"
 
-#define PASOS_POR_MILILITRO 200
-#define MILILITROS_DEL_VASO 400
+#define TIEMPO_POR_MILILITRO 30
+#define MILILITROS_DEL_VASO 500
 
 bool surtir(uint8_t slot, uint8_t porcentaje);
