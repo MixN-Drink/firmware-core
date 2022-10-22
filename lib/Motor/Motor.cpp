178a12a664e4b6cbdcf0e6f5fe33be2af72769dd
thomasverita <thomaseyen11@gmail.com>
2022-10-22 18:39:11 -0300

    agregados pines de bombas 1-3

--- lib/Motor/Motor.cpp
+++ lib/Motor/Motor.cpp
@@ -1,11 +1,14 @@
 #include "Motor.h"
 
-uint8_t motorPin[8] = {MOTOR_0_PIN,0,0,0,0,0,0,0};
+uint8_t motorPin[8] = {MOTOR_0_PIN,MOTOR_1_PIN,MOTOR_2_PIN,MOTOR_3_PIN,0,0,0,0};
 uint8_t motorARotar = 0;
 uint64_t tiempoFinal = 0;
 
 void motoresInit(void){
     pinMode(MOTOR_0_PIN, OUTPUT);
+    pinMode(MOTOR_1_PIN, OUTPUT);
+    pinMode(MOTOR_2_PIN, OUTPUT);
+    pinMode(MOTOR_3_PIN, OUTPUT);
 }
 void prepararMotor(uint8_t numeroDeMotor, uint32_t tiempo){
     motorARotar = numeroDeMotor;
--- lib/Motor/Motor.h
+++ lib/Motor/Motor.h
@@ -1,7 +1,10 @@
 #include <stdint.h>
 #include <Arduino.h>
 
-#define MOTOR_0_PIN 4
+#define MOTOR_0_PIN 8
+#define MOTOR_1_PIN 9
+#define MOTOR_2_PIN 10
+#define MOTOR_3_PIN 11
 
 void motoresInit(void);
 void prepararMotor(uint8_t numeroDeMotor, uint32_t tiempo);
